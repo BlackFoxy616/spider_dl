4654728889eb64bbbdccf97e927937bf308f64d3
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-10-21 16:31:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,117 @@
+from pyrogram import Client, filters
+import time,csv,os
+import yt_dlp
+from hurry.filesize import size
+from pyrogram.types import (ReplyKeyboardMarkup, InlineKeyboardMarkup,
+                            InlineKeyboardButton)
+
+
+
+
+
+#os.system("wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux")
+#os.system("mv yt-dlp_linux yt-dlp")
+
+
+
+
+api_id = 3702208
+api_hash = "3ee1acb7c7622166cf06bb38a19698a9"
+bot_token = "5969942271:AAFb6MJWug79qs96STXN7Y4eqXEcTfE8wjc"
+
+
+app = Client(
+    "ydl",
+    api_id=api_id, api_hash=api_hash,
+    bot_token=bot_token
+)
+
+
+
+def extract(yturl):
+    ydl = yt_dlp.YoutubeDL()
+    with ydl:
+        qualityList = []
+        r = ydl.extract_info(yturl, download=False)
+        """ with open ("dump.txt","w+") as file:
+             file.write(str(r))"""
+        for format in r['formats']:
+            if not "dash" in str(format['format']).lower():
+                if "filesize" in format and format['filesize'] != None and format['video_ext'] !="webm" :
+                  qualityList.append(" ".join([str(format['format']),size(format['filesize']),str(format['video_ext'])]))
+
+   
+   
+    return r["title"],qualityList
+
+
+
+
+
+@app.on_message(filters.command("dl"))
+async def start_command(client,message):
+         query = message.text.split()
+         button_list =[]
+         info = extract(query[1])
+         co=0
+         temp = []
+         for i in range(len(info[1])):
+             X = info[1][i].split(" ")
+             Y = X[3].replace("(","").replace(")","")+","+X[5]+","+X[4]
+             if X[3] == "only":
+                  Y = X[4].replace("(","").replace(")","")+", mp3 ,"+X[5]
+             temp.append(InlineKeyboardButton(id="1", message=Y, callback_data =info[1][i].split(" ")[0]+f"__{query[1]}"))
+             co+=1
+             if co%2==0 or i == len(info[1])-1:
+               button_list.append(temp)
+               temp=[]
+          
+         reply_markup=InlineKeyboardMarkup(button_list)
+         choice = await app.send_message(
+            message.chat.id,"Select The Format:",reply_markup=reply_markup)
+        
+
+
+
+
+
+
+
+def extract2(yturl):
+    ydl = yt_dlp.YoutubeDL()
+    with ydl:
+        qualityList = []
+        r = ydl.extract_info(yturl, download=False)
+        with open ("dump.txt","w+") as file:
+             file.write(str(r))
+
+   
+   
+    return r
+
+
+
+
+G =  extract2("https://youtube.com/playlist?list=PLxnyxqL6KA4jO2gQBRj560DGCFH4b5tIe&si=pTTeEk292wd1Qvy0")
+print(G)
+
+        
+
+
+
+
+@app.on_callback_query()
+async def answer(client, call):
+         await app.delete_messages(call.message.chat.id,call.message.id)
+         print(call.id)
+         data = call.data.split("__")
+         id = call.message.id
+         print(id)
+         os.system(f"yt-dlp -f {data[0]} --download-archive music.txt {data[1]}")
+         for i in os.listdir():
+                pass
+
+
+print("Bot Started ..!!")
+
+app.run()
