8f0ec27dbc0a2ad3777c22f4fd0826b5ce24be2e
MrSpidy616 <93417940+BlackFoxy616@users.noreply.github.com>
2023-10-28 22:47:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -475,8 +475,8 @@ def handle_document(client, message):
                  thumbnail = f"{i.replace('.mp4', '.png')}"
                  os.system(f'''vcsi "{i}" -g 2x1 --metadata-position hidden -o "{thumbnail}"''')
                  app.send_video(chat_id, video=i, caption=i, thumb=thumbnail)
-               elif i.endswith.("jpg") or i.endswith("png") :
-                 app.send_photo(chat_id, photo=i, caption=i)
+               elif i.endswith.("jpg") or i.endswith("png") or  i.endswith("jpeg"):
+                  app.send_photo(chat_id, photo=i, caption=i)
 
                try:
                   os.remove(i)
